b467c56b99ac2eec835c5294be01838578c2c4c9
vasya <trubetskoy.vasa@gmail.com>
2016-02-04 17:57:00 -0500

    Finished the setup of the GCE service, region, and sample processing. Need to write out the startup script.

--- basic_variant_calling.py
+++ basic_variant_calling.py
@@ -3,7 +3,9 @@
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 
-bam_list = 'data/2-bam.list'
+import itertools
+
+bam_list = 'data/2-sample_1000G_phase3_low-cov_gcs.list'
 
 def __main__():
   '''
@@ -19,20 +21,34 @@ def __main__():
   '''
 
   ## setup
+  # register a compute engine service
+  credentials = GoogleCredentials.get_application_default()
+  compute = discovery.build('compute', 'v1', credentials=credentials)
+
+  # read in bam files
+  sample_bams = { x.strip().split()[0]:x.strip().split()[1] for x in open(bam_list) }
+
+  # set up regions
+  #   we are testing with two random 5MB regions
+  regions = [ {'chr':'chr20', 'start':11000001, 'end':16000000},
+              {'chr':'chr20', 'start':36000001, 'end':41000001} ]
 
   ## pileup
   ##    inputs: BAMs in GCS, reference material on PD
   ##    outputs: PD with GLF for each sample x region
 
-  # write startup script for each sample x region node
+  region_names = [ '{0}-{1}-{2}'.format(x['chr'], str(x['start']), str(x['end'])) for x in regions ]
+  output_disks = [ '{0}-{1}'.format(x) for x in itertools.product(sample_bams.keys(), region_names) ]
 
-  # create instances for each sample x region
+  # write startup script for each sample x region node
   #   startup script:
   #     1. create and attach output disk
   #     2. attach disk with reference data 
   #     3. execute pileup command
   #     4. detach output and reference disks
   #     5. destroy yourself
+
+  # create instances for each sample x region
 
   
 
@@ -41,8 +57,6 @@ def __main__():
   ##    outputs: PD with vcf for each region
 
   # write startup script for each region
-
-  # create instances for each region
   #   startup script:
   #     1. attach input disk for each sample in RO mode
   #     2. create and attach output disk
@@ -50,6 +64,8 @@ def __main__():
   #     4. detach input and output disks
   #     5. destroy input disks
   #     6. destroy yourself
+
+  # create instances for each region
 
 if __name__ == '__main__':
   __main__()
